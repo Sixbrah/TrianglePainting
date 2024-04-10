5801f06279b7114c1209d83518f77f8676df748e
timof <timof@DESKTOP-G3P76R5>
2024-04-10 16:31:34 +0300

    Добавьте файлы проекта.

---
+++ TrianglePainting.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	char star = '*';
+	int height;
+	cin >> height;
+	for (height; height != 0; --height) {
+		cout << string(height, star) << endl;
+	}
+}
